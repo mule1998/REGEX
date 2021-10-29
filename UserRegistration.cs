36efb4a5934fcc27ff752b3acaa82c887e033afe
Shubham Mule <shubhambmule@gmail.com>
2021-10-30 00:58:58 +0530

    Checking in password should be one numeric value

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -11,7 +11,7 @@ namespace DAY_19_UserRegistration
         public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public static string Regex_Number = "^[0-9]{2}[ ][6-9][0-9]{9}$";
-        public static string Regex_Password = "^((?=.*[A-Z])(?=.{8,}))";
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.{8,}))";
 
 
         public static bool ValidateFirstName(string FirstName)
@@ -64,7 +64,7 @@ namespace DAY_19_UserRegistration
         {
             if (Regex.IsMatch(number, Regex_Number) == false)
             {
-                Console.WriteLine("Enter valid Mobile number!");
+                Console.WriteLine("Enter valid number!");
             }
         }
 
@@ -78,7 +78,7 @@ namespace DAY_19_UserRegistration
             if (Regex.IsMatch(password, Regex_Password) == false)
             {
                 Console.WriteLine("Enter valid password. It should contain minimum 8 character," +
-                    "atleast one upper case character");
+                    "atleast one upper case character and atleast one numeric value");
             }
         }
 
